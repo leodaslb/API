a1e8bc00c40d39ecf5ea9b6b71daf6a414c2fc7c
leodaslb <leodaslb@gmail.com>
2025-04-25 22:55:38 -0300

    Update app.py rota

--- API - 1 Semestre/src/app.py
+++ API - 1 Semestre/src/app.py
@@ -145,7 +145,5 @@ def carregar_arquivo():
 ## fechamento
 
 if __name__ == "__main__":
-
-port = int(os.environ.get('PORT', 5000))
-
-app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
